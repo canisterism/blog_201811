31c87fe342538d821ad0b09abaf0bfd83361b724
canisterism <ohtthstc@gmail.com>
2018-11-07 13:58:20 +0900

    Revert "[fix]fix main.js"
    This reverts commit f89a600f4550687156e43a22c1866dcd3147aec0.
    
    Revert "[fix]delete env files"
    
    This reverts commit b43efd2eefa37704651d9e6863cefdda8d5d10b7.

---
+++ config/dev.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const prodEnv = require('./prod.env')
+
+module.exports = merge(prodEnv, {
+  NODE_ENV: '"development"',
+  VUE_APP_BUTTER_API_KEY: '"YOUR API KEY"'
+
+})
---
+++ config/prod.env.js
@@ -0,0 +1,5 @@
+'use strict'
+module.exports = {
+  NODE_ENV: '"production"',
+  VUE_APP_BUTTER_API_KEY: '"YOUR API KEY"'
+}
